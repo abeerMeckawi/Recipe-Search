f1fb049e98c45723cc9f2c15cfa080f7433c2244
Abeer Meckawi <abeer.meckawii@gmail.com>
2021-07-28 23:53:51 -0700

    Initial Commit

---
+++ IOS Task-SoftxpertTests/IOS_Task_SoftxpertTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_Task_SoftxpertTests.swift
+//  IOS Task-SoftxpertTests
+//
+//  Created by Abeer Meckawi on 28/07/2021.
+//
+
+import XCTest
+@testable import IOS_Task_Softxpert
+
+class IOS_Task_SoftxpertTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
