3825327045bfa0fc33d71055916e9c11c29e200c
Abeer Meckawi <abeer.meckawii@gmail.com>
2021-07-29 02:41:29 -0700

    Initial Commit

---
+++ Pods/Target Support Files/Pods-IOS Task-Softxpert-IOS Task-SoftxpertUITests/Pods-IOS Task-Softxpert-IOS Task-SoftxpertUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_Task_Softxpert_IOS_Task_SoftxpertUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_Task_Softxpert_IOS_Task_SoftxpertUITests
+@end
---
+++ Pods/Target Support Files/Pods-IOS Task-SoftxpertTests/Pods-IOS Task-SoftxpertTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_Task_SoftxpertTests : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_Task_SoftxpertTests
+@end
